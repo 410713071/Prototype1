e4333fda0e71a9a89eecf1211bb48030f813c5cf
410713071 <410713071@gms.ndhu.edu.tw>
2020-10-20 11:46:45 +0800

    1020 rigibody added

--- Assets/scripts/play_controller.cs
+++ Assets/scripts/play_controller.cs
@@ -13,7 +13,7 @@ public class play_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime*15); ;
 
          
     }
